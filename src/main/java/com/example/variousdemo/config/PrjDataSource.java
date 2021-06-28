50490ddbeefe9dfb0adf1024b0aa5449de21c1d6
penghc <295836675@qq.com>
2021-06-28 09:16:14 +0800

    提交基础配置

---
+++ src/main/java/com/example/variousdemo/config/PrjDataSource.java
@@ -0,0 +1,14 @@
+package com.example.variousdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @className: PrjDataSource
+ * @description: 项目数据源
+ * @created: 2021/06/27 11:31
+ */
+@Configuration
+public class PrjDataSource {
+
+
+}
