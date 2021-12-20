c889150f60966f982b6b8ae7eef9908f3fe0a6d9
penghaocong <penghaocong@bluemoon.com.cn>
2021-12-20 09:37:40 +0800

    2222

--- src/test/java/com/example/variousdemo/rabbitmq/HelloTest.java
+++ src/test/java/com/example/variousdemo/rabbitmq/HelloTest.java
@@ -15,7 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HelloTest {
-
+    private String ddsd;
     @Autowired
     private HelloSender helloSender;
 
