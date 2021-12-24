ddc469037025a538b542bbcff7b859c9f27b6cbe
penghaocong <penghaocong@bluemoon.com.cn>
2021-12-24 09:47:41 +0800

    Revert "提交各类算法demo+实例"
    This reverts commit b67bf8848ed3171bb101b233e21e16e24b0566f2.

--- src/main/java/com/example/variousdemo/singleton/DCLSingleton.java
+++ src/main/java/com/example/variousdemo/singleton/DCLSingleton.java
@@ -2,9 +2,6 @@ package com.example.variousdemo.singleton;
 
 import lombok.Synchronized;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @className: DCLSingleton
  * @description: DCL（Double Check Lock）加锁的单例模式
@@ -25,19 +22,5 @@ public class DCLSingleton {
             }
         }
         return singleton;
-    }
-
-
-    public static void main(String[] args) {
-        Map map = new HashMap<>();
-        int target = 9;
-        int a[] = {1,2,3,4,5,6,7,8,9};
-        for(int i = 0 ; i <  a.length ; i++){
-            if(map.containsKey(9-a[i])){
-                System.out.println("数组下标为" + i + ","+map.get(9-a[i]));
-            } else {
-                map.put(a[i],i);
-            }
-        }
     }
 }
